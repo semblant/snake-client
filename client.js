87e9a8828e6a748493852acb7d1148d8d66f683c
semblant <robynhumphreys7_4@hotmail.com>
2024-10-22 22:28:02 -0600

    added setup input and handle user input functions (basic)

--- client.js
+++ client.js
@@ -12,6 +12,19 @@ const connect = function() {
   conn.on('connect', () => {
     console.log("Successfully connected to game server!");
     conn.write('Name: FLY');
+
+
+    // TEMPORARY CODE
+    //setInterval(() => {
+    //  conn.write('Move: up'); // moves the snake up 1
+    //}, 50)
+    //conn.write('Move: up'); // moves the snake up 1
+    //conn.write('Move: down'); // moves the snake right 1
+    //conn.write('Move.down'); // moves the snake down 1
+    //conn.write('Move: left'); // moves the snake left 1
+
+
+
   });
 
   conn.on('data', (data) => {
@@ -22,4 +35,4 @@ const connect = function() {
 };
 
 
-module.exports = { connect }
+module.exports = { connect };
