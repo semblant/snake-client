67aa409a52981ed751e83ccf95e6727a7677e854
semblant <robynhumphreys7_4@hotmail.com>
2024-10-23 22:08:45 -0600

    handle client errors -> Snake runs into wall, other snake etc. to close

--- client.js
+++ client.js
@@ -1,22 +1,33 @@
+const { IP, PORT } = require('./constants');
+const { clients } = require('./constants');
+
+
 // establishes connection with the game server
 const connect = function() {
   const net = require('net');
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf-8");
 
-  conn.on('connect', () => {
-    console.log("Successfully connected to game server!");
-    conn.write('Name: FLY');
+  conn.on('connect', (connection) => {
+    console.log("Successfully connected to game server!\n");
+    conn.write('Name: FLY'); // could turn this into constant variable - ask user to define name when connected to server?
   });
 
   conn.on('data', (data) => {
     console.log(data);
+    if (data === 'you ded cuz you idled\n') {
+      process.exit();
+    }
   });
+
+  conn.on('error', () => {
+    process.exit()
+  })
 
   return conn;
 };
