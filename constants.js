be36bb757dcd300e9f2acf9c979f127afbe0ab61
semblant <robynhumphreys7_4@hotmail.com>
2024-10-23 22:13:29 -0600

    adding file for all constants

---
+++ constants.js
@@ -0,0 +1,22 @@
+const IP = 'localhost';
+const PORT = 50541;
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+const MESSAGES = {
+  g: 'Say: hello!!!',
+  y: 'Say: a red pixel! YUM',
+  i: "Say: i'm a snekkk",
+  b: 'Say: Bye everyone :)',
+}
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGES,
+};
