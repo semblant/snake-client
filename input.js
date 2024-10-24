003073b2b248d632c52e363576b8deab26869c7a
semblant <robynhumphreys7_4@hotmail.com>
2024-10-23 22:12:15 -0600

    Handled case for continuing direction if the opposite key is pressed

--- input.js
+++ input.js
@@ -1,3 +1,11 @@
+const { clearInterval } = require('timers');
+const { MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGES,
+} = require('./constants')
+
 // Stores the active TCP connection object
 let connection;
 
@@ -11,41 +19,81 @@ const setupInput = (conn) => {
   stdin.setEncoding('utf8'); // utf8 is set so we can read the text data that is input
   stdin.resume(); // resume stdin so the program can listen for input
 
+  // Call user input function to handle user input
   stdin.on('data', (key) => handleUserInput(key));
   return stdin; // return the stdin object so we can use it elsewhere
 };
 
-const handleUserInput = (key) => {
+const movementDirection = (key) => {
   switch (key) {
+    // Movement cases
+    case MOVE_UP_KEY:
+      connection.write('Move: up');
+      break;
+    case MOVE_LEFT_KEY:
+      connection.write('Move: left');
+      break;
+    case MOVE_DOWN_KEY:
+      connection.write('Move: down');
+      break;
+    case MOVE_RIGHT_KEY:
+      connection.write('Move: right');
+      break;
+  }
+};
 
+const messageInput = key => {
+  switch(key) {
+    // Chat message cases
+    case MESSAGES['g']:
+      connection.write(MESSAGES['g']);
+      break;
+    case MESSAGES['y']:
+      connection.write(MESSAGES['y']);
+      break;
+    case MESSAGES['i']:
+      connection.write(MESSAGES['i']);
+      break;
+    case MESSAGES['b']:
+      connection.write(MESSAGES['b']);
+      break;
+  }
+};
+
+// Stores active movement key
+let moveInterval;
+let direction;
+const opposites = {
+  'w': 's',
+  'a': 'd',
+  's': 'w',
+  'd': 'a',
+};
+
+const handleUserInput = (key) => {
   // Exit case
-  case '\u0003':
+  if (key ===  '\u0003') {
     process.stdout.write("Bye!\n"); // write a message in the console upon exit
     process.exit();
-    break;
-  // Movement cases
-  case 'w':
-    connection.write('Move: up');
-    break;
-  case 'a':
-    connection.write('Move: left');
-    break;
-  case 's':
-    connection.write('Move: down');
-    break;
-  case 'd':
-    connection.write('Move: right');
-    break;
 
-  // Message cases
-  case 'y':
-    connection.write('Say: red pixel YUM');
-    break;
-  case 'i':
-    connection.write('Say: im a slithery snek');
-    break;
-  case 'b':
-    connection.write('Say: Bye everyone :)');
+    // Movement cases
+  } else if ([MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY].includes(key)) {
+    // Case: direction is up 'w' and user presses down 'd', direction continues up
+    if (direction !== opposites[key]) {
+      direction = key; // store current direction
+
+      // Check if there is an exisiting movement interval
+      if (moveInterval) {
+        clearInterval(moveInterval);
+      }
+      // Set new interval with new movement key
+      moveInterval = setInterval(() => {
+      movementDirection(key);
+    }, 100);
+    }
+    // Message cases
+  } else if (MESSAGES[key]) {
+    messageInput(MESSAGES[key]);
   }
 };
 
